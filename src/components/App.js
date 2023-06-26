15d6150b569d931f87e8abcfccd9d4427bdc3833
Yashjain0007 <yashjain3446@gmail.com>
2023-06-26 23:47:31 +0530

    submitting code-Mon Jun 26 2023 23:47:28 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>Enter your name:</h1>
+        <p>Enter your name:</p>
         <input type="text" onChange={(e)=> setName(e.target.value)}/>
 
         {
